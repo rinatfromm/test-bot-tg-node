f9a3128c72bdf65aa93e53e63d34651fe421e4b6
Rinat Kov <frommrinat@gmail.com>
2024-03-15 23:49:58 +0100

    correction

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  if (text === "/start") {
-    await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
-      reply_markup: {
-        keyboard: [
-          [{ text: "Ваша карточка", web_app: { url: webAppUrl  + "/card"} }],
-        ],
-      },
-    });
+  if(text === '/start') {
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+        reply_markup: {
+            keyboard: [
+                [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+            ]
+        }
+    })
 
-    await bot.sendMessage(chatId, "Создай форму", {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: "Заполнить форму", web_app: { url: webAppUrl } }],
-        ],
-      },
-    });
-  }
+    await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+            ]
+        }
+    })
+}
 
   if (msg?.web_app_data?.data) {
     try {
