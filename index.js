0c8b101c57f98d5bdbb94bddbaa43b6ffeaaea60
Rinat Kov <frommrinat@gmail.com>
2024-03-16 12:13:32 +0100

    correction

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ bot.on("message", async (msg) => {
       {
         reply_markup: {
           keyboard: [
-            [{ text: "Заполнить форму",, web_app: { url: webAppUrl } }],
+            [{ text: "Заполнить форму", web_app: { url: webAppUrl } }],
           ],
         },
       }
