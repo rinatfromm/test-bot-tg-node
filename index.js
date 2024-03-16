9dd2f6b5be776483719a669d1235beb4126b9899
Rinat Kov <frommrinat@gmail.com>
2024-03-16 11:04:18 +0100

    correction

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ bot.on("message", async (msg) => {
   if (text === "/start") {
     await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
       reply_markup: {
-        keyboard: [
-          [{ text: "Заполнить форму", web_app: { url: webAppUrl  } }],
-        ],
+        keyboard: [[{ text: "Заполнить форму", web_app: { url: webAppUrl } }]],
       },
     });
 
@@ -23,7 +21,7 @@ bot.on("message", async (msg) => {
       {
         reply_markup: {
           inline_keyboard: [
-            [{ text: "Сделать заказ", web_app: { url: webAppUrl + "/card"} }],
+            [{ text: "Сделать заказ", web_app: { url: webAppUrl + "/card" } }],
           ],
         },
       }
@@ -37,7 +35,11 @@ bot.on("message", async (msg) => {
       await bot.sendMessage(chatId, "Спасибо за обратную связь!");
       await bot.sendMessage(chatId, "Ваше имя: " + data?.userName);
       await bot.sendMessage(chatId, "Ваш возраст: " + data?.userAge);
-      await bot.sendMessage(chatId, "Ваше фото: " + data?.userFoto);
+
+      if (data.userFoto) {
+        await bot.sendPhoto(chatId, data.userFoto);
+      }
+
       console.log(data);
       setTimeout(async () => {
         await bot.sendMessage(chatId, "Всю информацию вы получите в этом чате");
