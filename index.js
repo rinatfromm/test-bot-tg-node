0482ee848d472930281e9355493449f4943f4e99
Rinat Kov <frommrinat@gmail.com>
2024-03-17 11:40:42 +0100

    correction

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const token = "7052992202:AAGTD6eOEU95USn7BkoXZmNTAM9Ij0-TmYM";
 const webAppUrl = "https://velvety-custard-289c52.netlify.app/";
 const bot = new TelegramBot(token, { polling: true });
 
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Привет! Я ваш Telegram-бот.');
+});
+
+
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
@@ -19,14 +24,30 @@ bot.on("message", async (msg) => {
   if (msg?.web_app_data?.data) {
     try {
       const data = JSON.parse(msg?.web_app_data?.data);
-      console.log(data);
+     
       await bot.sendMessage(chatId, "Спасибо за обратную связь!");
       await bot.sendMessage(chatId, "Ваше имя: " + data?.userName);
       await bot.sendMessage(chatId, "Ваш возраст: " + data?.userAge);
 
-      //   if (data.userFoto) {
-      //     await bot.sendPhoto(chatId, data.userFoto);
-      // }
+      await bot.sendMessage(chatId, "Отправте ваше фото");
+
+      if (msg.photo) {
+        // Получаем информацию о фотографии
+        const photo = msg.photo[msg.photo.length - 1];
+        const photoId = photo.file_id;
+    
+        // Запрашиваем информацию о фотографии
+        bot.getFile(photoId).then((fileInfo) => {
+          // Получаем ссылку на файл
+          const fileUrl = `https://api.telegram.org/file/bot${token}/${fileInfo.file_path}`;
+          
+          // Отправляем ссылку на изображение в чат
+          bot.sendMessage(chatId, `Вы отправили фотографию: ${fileUrl}`);
+        }).catch((err) => {
+          console.error('Ошибка получения информации о файле:', err);
+          bot.sendMessage(chatId, 'Произошла ошибка при получении информации о фотографии.');
+        });
+      }
 
       console.log(data);
       setTimeout(async () => {
